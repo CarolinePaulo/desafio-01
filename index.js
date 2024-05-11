4da18910a155e9a6b1be181219f7a409ddab8b97
CarolinePaulo <147195940+CarolinePaulo@users.noreply.github.com>
2024-05-10 22:45:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,27 @@ let HP = 0
 let XP = 2001
 const rank = "O herói de nome " + nomeDoHeroi + " está no nível de "
 
-if(XP < 1000) {
-    console.log(rank + "Ferro.")} 
-    else if (XP > 1000 && XP < 2001){
-    console.log(rank + "Bronze.")} 
-    else if (XP > 2000 && XP < 5001){
-    console.log(rank + "Prata.")}
-    else if (XP > 6000 && XP < 7001){
-    console.log(rank + "Ouro.")}
-    else if (XP > 7000 && XP < 8001){
-    console.log(rank + "Platina.")}
-    else if (XP > 8000 && XP < 9001){
-    console.log(rank + "Ascendente.")}
-    else if (XP > 9000 && XP < 10001){
-    console.log(rank + "Imortal.")}
-    else if (XP > 10000){
-    console.log(rank + "Radiante.")}
+if (XP < 1000) {
+    console.log(rank + "Ferro.")
+}
+else if (XP > 1000 && XP < 2001) {
+    console.log(rank + "Bronze.")
+}
+else if (XP > 2000 && XP < 5001) {
+    console.log(rank + "Prata.")
+}
+else if (XP > 6000 && XP < 7001) {
+    console.log(rank + "Ouro.")
+}
+else if (XP > 7000 && XP < 8001) {
+    console.log(rank + "Platina.")
+}
+else if (XP > 8000 && XP < 9001) {
+    console.log(rank + "Ascendente.")
+}
+else if (XP > 9000 && XP < 10001) {
+    console.log(rank + "Imortal.")
+}
+else if (XP > 10000) {
+    console.log(rank + "Radiante.")
+}
